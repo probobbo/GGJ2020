38b1ce798e56cddcb3cb608cc224a8c5976ddeb6
Marcello <probobbo@gmail.com>
2020-01-31 22:52:59 +0100

    Bozza meccanica del joint

---
+++ Assets/Scripts/Statue/ConnectionJoint.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using UnityEngine;
+
+namespace Statue
+{
+    [RequireComponent(typeof(Rigidbody))]
+    public class ConnectionJoint : MonoBehaviour
+    {
+        private StatuePiece _parentPiece;
+        
+        private void Awake()
+        {
+            var parent = transform.parent;
+            if (parent == null || parent.GetComponent<StatuePiece>()==null)
+                Destroy(gameObject);
+            _parentPiece = parent.GetComponent<StatuePiece>();
+        }
+
+        private bool IsParentConnected()
+        {
+            return _parentPiece.IsPieceConnected();
+        }
+
+        private void ConnectToParent(Transform parent)
+        {
+            _parentPiece.ConnectObject(parent);
+        } 
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.CompareTag("Joint"))
+            {
+                var otherJoint = other.GetComponent<ConnectionJoint>();
+                if (otherJoint != null && IsParentConnected() && !otherJoint.IsParentConnected())
+                {
+                    otherJoint.ConnectToParent(_parentPiece.transform);
+                }
+            }
+        }
+    }
+}
