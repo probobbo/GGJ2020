f7d60b42f3cfd00e3649f5b5fdcb8e0058f4e045
Nicola Spolador <gench23@gmail.com>
2020-02-01 17:38:50 +0100

    tentative fix hand

---
+++ Assets/Scripts/Hands/ClawGrabber.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClawGrabber : OVRGrabber
+{
+    private Claw _claw;
+
+    protected override void Start()
+    {
+        base.Start();
+        _claw = GetComponent<Claw>();
+    }
+
+    protected override void GrabBegin()
+    {
+        base.GrabBegin();
+        _claw.ActivateHand();
+    }
+
+    protected override void GrabEnd()
+    {
+        base.GrabEnd();
+        _claw.DeactivateHand();
+    }
+}
