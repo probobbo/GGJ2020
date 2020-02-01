5d08940834d86c5d9b0fc71ddca57f788b7159f3
Nicola Spolador <gench23@gmail.com>
2020-02-02 00:30:48 +0100

    fix prefab pistole

--- Assets/Scripts/Hands/Bat.cs
+++ Assets/Scripts/Hands/Bat.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using Hands;
 using Statue;
 using UnityEngine;
 
@@ -8,29 +9,25 @@ public class Bat : RoboHand
     [SerializeField] private float batStrength;
     private bool _isActive;
 
-    protected override void Start()
-    {
-        base.Start();
-        Rb.mass = strength;
-    }
-
     public override void ActivateHand()
     {
         if (_isActive) return;
         base.ActivateHand();
         remainingUsages--;
         _isActive = true;
-        Rb.mass *= batStrength;
+        strength *= batStrength;
     }
 
     protected override void OnCollisionEnter(Collision other)
     {
-        Debug.Log("colliso");
-        base.OnCollisionEnter(other);
         var statuePiece = other.gameObject.GetComponent<StatuePiece>();
         if (statuePiece == null) return;
         
+        var vel = _velocityEstimator.GetVelocityEstimate();
+        Debug.Log("nigga collide " + vel);
+        statuePiece.ApplyForce(vel * strength);
+        if(_isActive)
+            strength /= batStrength;
         _isActive = false;
-        Rb.mass = strength;
     }
 }
