fad34e2cca868f0d36cdfa45744589e5f187e7e0
mdallapi <massimo.dallapieta@hotmail.it>
2020-02-01 16:23:43 +0100

    aggiunte mano cannone/pistola base (no modello)

--- Assets/Scripts/Hands/Gun.cs
+++ Assets/Scripts/Hands/Gun.cs
@@ -4,8 +4,13 @@ using UnityEngine;
 
 public class Gun : RoboHand
 {
+    public GameObject firePoint;
+    public GameObject bullet;
+    public float bulletSpeed = 10f;
     public override void ActivateHand()
     {
-    
+        var bull = Instantiate(bullet, firePoint.transform.position,firePoint.transform.rotation);
+        bull.GetComponent<Rigidbody>().AddForce(transform.forward * bulletSpeed);
+        base.ActivateHand();
     }
 }
