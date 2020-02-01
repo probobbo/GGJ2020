0c1d724532d56ddd419b246d97bc92ae70a58a67
Giacomo Garoffolo <giacomo.garoffolo@gmail.com>
2020-02-01 11:27:20 +0100

    Aggiunto modello in scena menu

---
+++ Assets/Scripts/BeginButton.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using DG.Tweening;
+using UnityEngine;
+
+public class BeginButton : MonoBehaviour
+{
+    //[SerializeField] [EventRef] private string sfx;
+    private bool _alreadyTriggered = false;
+    private CanvasGroup _canvasGroup;
+
+    
+    private void Start()
+    {
+        _alreadyTriggered = false;
+        _canvasGroup = GetComponentInParent<CanvasGroup>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!other.CompareTag("Hand")) return;
+        if (_alreadyTriggered) return;
+        _alreadyTriggered = true;
+        
+        //AudioManager.PlayOneShotAudio(sfx, gameObject);
+        _canvasGroup.DOFade(0, 1f);
+    }
+}
