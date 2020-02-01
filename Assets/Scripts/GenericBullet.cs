9dc227b1898e99f39e13251af44c38768b5517dc
mdallapi <massimo.dallapieta@hotmail.it>
2020-02-01 15:00:14 +0100

    aggiunto proiettile base

---
+++ Assets/Scripts/GenericBullet.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Statue;
+using UnityEngine;
+
+public class GenericBullet : MonoBehaviour
+{
+    public float speed;
+    public float strength = 1f;
+    public float area;
+
+    private void Start()
+    {
+    }
+
+    protected virtual void OnCollisionEnter(Collision other)
+    {
+        var statuePiece = other.gameObject.GetComponent<StatuePiece>();
+        if (statuePiece == null) return;
+
+        if (!statuePiece.IsOnTheFloor) return;
+        var impulse = new Vector3(other.impulse.x, -other.impulse.y, other.impulse.z);
+        statuePiece.ApplyForce(impulse * strength);
+    }
+}
