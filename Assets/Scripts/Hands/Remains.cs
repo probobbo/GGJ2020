2a7908446a7da918c4f305cf8d8d338a8bed917c
mdallapi <massimo.dallapieta@hotmail.it>
2020-02-02 12:16:42 +0100

    aggiunta armachecade, importati particellari gpg

---
+++ Assets/Scripts/Hands/Remains.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Remains : MonoBehaviour
+{
+    public float deathTimer = 5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(Delet());
+    }
+
+    // Update is called once per frame
+    private IEnumerator Delet()
+    {
+        yield return new WaitForSeconds(deathTimer);
+        Destroy(gameObject);
+    }
+}
