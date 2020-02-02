f174c9db9df013eca884ee9ec95c670c2f98589a
Giacomo Garoffolo <giacomo.garoffolo@gmail.com>
2020-02-02 12:52:32 +0100

    suono per i remain

--- Assets/Scripts/Statue/ProgressLights.cs
+++ Assets/Scripts/Statue/ProgressLights.cs
@@ -9,20 +9,23 @@ public class ProgressLights : MonoBehaviour
     [SerializeField]
     private List<MeshRenderer> lights = new List<MeshRenderer>(6);
 
+    [SerializeField] private Color colorRed;
+    [SerializeField] private Color colorGreen;
+
     private static readonly int EMISSION_COLOR = Shader.PropertyToID("_EmissionColor");
 
     // Start is called before the first frame update
     void Start()
     {
-        lights.ForEach(meshRenderer => meshRenderer.material.color = Color.red);
+        lights.ForEach(meshRenderer => meshRenderer.material.color = colorRed);
         EventManager.Instance.onPieceConnected.AddListener(LightLight);
     }
 
     private void LightLight()
     {
-        var currentLight = lights.First(meshRenderer => meshRenderer.material.color == Color.red);
-            currentLight.material.color = Color.green;
-            currentLight.material.SetColor(EMISSION_COLOR, Color.green);
+        var currentLight = lights.First(meshRenderer => meshRenderer.material.color == colorRed);
+            currentLight.material.color = colorGreen;
+            currentLight.material.SetColor(EMISSION_COLOR, colorGreen);
             currentLight.material.EnableKeyword("_EMISSION");
         AudioManager.PlayOneShotAudio("event:/SOUNDFX/SFX_CompleteTask",gameObject);
     }
