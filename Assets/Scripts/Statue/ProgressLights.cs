c7f6679566616e1db9edb86a92e4e271ebe26460
mdallapi <massimo.dallapieta@hotmail.it>
2020-02-02 10:30:06 +0100

    aggiunto script progresso

---
+++ Assets/Scripts/Statue/ProgressLights.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using Managers;
+using UnityEngine;
+
+public class ProgressLights : MonoBehaviour
+{
+    private List<MeshRenderer> lights;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        lights = GetComponentsInChildren<MeshRenderer>().ToList();
+        EventManager.Instance.onPieceConnected.AddListener(LightLight);
+    }
+
+    private void LightLight()
+    {
+        lights.First(meshRenderer => meshRenderer.material.color == Color.red).material.color = Color.green;
+    }
+}
