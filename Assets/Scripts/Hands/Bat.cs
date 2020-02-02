20bcf9b9219e0b35a7dafe4ca22575621b926240
Nicola Spolador <gench23@gmail.com>
2020-02-02 13:52:33 +0100

    mazza magenta

--- Assets/Scripts/Hands/Bat.cs
+++ Assets/Scripts/Hands/Bat.cs
@@ -8,6 +8,19 @@ public class Bat : RoboHand
 {
     [SerializeField] private float batStrength;
     private bool _isActive;
+    [SerializeField] private MeshRenderer mesh;
+    private Material _mat;
+    private static readonly int EmissionColor = Shader.PropertyToID("_EmissionColor");
+    private Color col;
+    private static readonly int mainColor = Shader.PropertyToID("_Color");
+
+    protected override void Start()
+    {
+        base.Start();
+        _mat = mesh.material;
+        col = _mat.GetColor(mainColor);
+
+    }
 
     public override void ActivateHand()
     {
@@ -16,6 +29,9 @@ public class Bat : RoboHand
         remainingUsages--;
         _isActive = true;
         strength *= batStrength;
+        _mat.EnableKeyword("_EMISSION");
+        _mat.SetColor(EmissionColor, Color.magenta);
+        _mat.SetColor(mainColor, Color.magenta);
     }
 
     protected override void OnCollisionEnter(Collision other)
@@ -25,8 +41,14 @@ public class Bat : RoboHand
         
         var vel = _velocityEstimator.GetVelocityEstimate();
         statuePiece.ApplyForce(vel * strength);
-        if(_isActive)
+        if (_isActive)
+        {
+            _mat.DisableKeyword("_EMISSION");
+            _mat.SetColor(EmissionColor, Color.white);
+            _mat.SetColor(mainColor, col);
             strength /= batStrength;
+        }
+
         _isActive = false;
     }
 }
