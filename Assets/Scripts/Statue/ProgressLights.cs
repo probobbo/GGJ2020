b1fa59091dfbd576f865e1274c80d6562e81a723
mdallapi <massimo.dallapieta@hotmail.it>
2020-02-02 10:48:35 +0100

    modificata scena main con luci

--- Assets/Scripts/Statue/ProgressLights.cs
+++ Assets/Scripts/Statue/ProgressLights.cs
@@ -6,12 +6,14 @@ using UnityEngine;
 
 public class ProgressLights : MonoBehaviour
 {
-    private List<MeshRenderer> lights;
+    [SerializeField]
+    private List<MeshRenderer> lights = new List<MeshRenderer>(6);
     
     // Start is called before the first frame update
     void Start()
     {
         lights = GetComponentsInChildren<MeshRenderer>().ToList();
+        lights.ForEach(meshRenderer => meshRenderer.material.color = Color.red);
         EventManager.Instance.onPieceConnected.AddListener(LightLight);
     }
 
